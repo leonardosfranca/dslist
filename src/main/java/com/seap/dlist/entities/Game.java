a1d080d7620b5aac8da7563a2ab18f899d3ab74a
leoDev <leonardo5155@gmail.com>
2023-05-10 00:56:48 -0300

    properties, Game, ORM

---
+++ src/main/java/com/seap/dlist/entities/Game.java
@@ -0,0 +1,24 @@
+package com.seap.dlist.entities;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "td_game")
+public class Game {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    @Column(name = "game_year")
+    private Integer year;
+    private String genre;
+    private String platforms;
+    private Double score;
+    private String imgUrl;
+    private String shortDescription;
+    private String longDescription;
+}
