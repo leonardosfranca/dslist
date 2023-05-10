5f06c12db0eae5ce2091b2739c5fb2d02e65fa80
leoDev <leonardo5155@gmail.com>
2023-05-10 00:41:28 -0300

    Criando projeto

---
+++ src/test/java/com/seap/dlist/DlistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seap.dlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DlistApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
